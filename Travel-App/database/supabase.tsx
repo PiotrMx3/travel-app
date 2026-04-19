3fab9ab3df2901faf5943f66e913a7445c5fe6e0
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-04-19 19:20:20 +0200

    feat: database init

---
+++ Travel-App/database/supabase.tsx
@@ -0,0 +1,16 @@
+import {Database} from "@/types/database.types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {createClient} from "@supabase/supabase-js";
+
+export const supabase = createClient<Database>(
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  },
+);
